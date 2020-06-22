5b72168a7e4972b892cd11080d107682477829f9
yaffa <yaffa@DESKTOP-VRQ5167>
2020-06-22 14:09:44 +0300

    adding joystick to control screen

--- app/src/main/java/com/example/flightmobileapp/ControlScreenActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/ControlScreenActivity.kt
@@ -1,49 +1,17 @@
 package com.example.flightmobileapp
 
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.ShapeDrawable
-import android.graphics.drawable.shapes.OvalShape
-import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.control_screen.*
 
-class ControlScreenActivity: AppCompatActivity() {
+class ControlScreenActivity: AppCompatActivity(), JoystickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val joystick = MyJoystick(this)
         setContentView(R.layout.control_screen)
+    }
 
-        val bitmap: Bitmap = Bitmap.createBitmap(700, 1000, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        var joystick = MyJoystick(this)
-        joystick.draw(canvas)
-
-        /*var shapeDrawable: ShapeDrawable*/
-
-//        // rectangle positions
-//        var left = 200
-//        var top = 650
-//        var right = 500
-//        var bottom = 900
-//
-//        // draw oval shape to canvas
-//        shapeDrawable = ShapeDrawable(OvalShape())
-//        shapeDrawable.setBounds( left, top, right, bottom)
-//        shapeDrawable.getPaint().setColor(Color.parseColor("darkgrey"))
-//        shapeDrawable.draw(canvas)
-
-
-        // now bitmap holds the updated pixels
-
-        // set bitmap as background to ImageView
-//        imageV.background = BitmapDrawable(getResources(), bitmap)
-
+    override fun onJoystickMoved(xPercent: Float, yPercent: Float, id: Int) {
+        Log.d("Joystick", "X percent: $xPercent Y percent: $yPercent")
     }
 }
--- app/src/main/java/com/example/flightmobileapp/MyJoystick.kt
+++ app/src/main/java/com/example/flightmobileapp/MyJoystick.kt
@@ -1,7 +1,6 @@
 package com.example.flightmobileapp
 
 import android.content.Context
-import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PorterDuff
@@ -11,9 +10,11 @@ import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.View
 import android.view.View.OnTouchListener
-import kotlin.math.sqrt
 
 
+/**
+ * Created by Daniel on 7/25/2016.
+ */
 class MyJoystick : SurfaceView, SurfaceHolder.Callback, OnTouchListener {
     private var centerX = 0f
     private var centerY = 0f
@@ -24,15 +25,14 @@ class MyJoystick : SurfaceView, SurfaceHolder.Callback, OnTouchListener {
     private fun setupDimensions() {
         centerX = width / 2.toFloat()
         centerY = height / 2.toFloat()
-        baseRadius = width.coerceAtMost(height) / 3.toFloat()
-        hatRadius = width.coerceAtMost(height) / 7.toFloat()
+        baseRadius = Math.min(width, height) / 3.toFloat()
+        hatRadius = Math.min(width, height) / 5.toFloat()
     }
 
     constructor(context: Context?) : super(context) {
         holder.addCallback(this)
         setOnTouchListener(this)
-        if (context is JoystickListener) joystickCallback =
-            context
+        if (context is JoystickListener) joystickCallback = context
     }
 
     constructor(
@@ -42,8 +42,7 @@ class MyJoystick : SurfaceView, SurfaceHolder.Callback, OnTouchListener {
     ) : super(context, attributes, style) {
         holder.addCallback(this)
         setOnTouchListener(this)
-        if (context is JoystickListener) joystickCallback =
-            context
+        if (context is JoystickListener) joystickCallback = context
     }
 
     constructor(context: Context?, attributes: AttributeSet?) : super(
@@ -52,21 +51,61 @@ class MyJoystick : SurfaceView, SurfaceHolder.Callback, OnTouchListener {
     ) {
         holder.addCallback(this)
         setOnTouchListener(this)
-        if (context is JoystickListener) joystickCallback =
-            context
+        if (context is JoystickListener) joystickCallback = context
     }
 
     private fun drawJoystick(newX: Float, newY: Float) {
         if (holder.surface.isValid) {
-            val myCanvas: Canvas = this.holder.lockCanvas() //Stuff to draw
+            val myCanvas = this.holder.lockCanvas() //Stuff to draw
             val colors = Paint()
+            myCanvas.drawColor(
+                Color.TRANSPARENT,
+                PorterDuff.Mode.CLEAR
+            ) // Clear the BG
+
+            //First determine the sin and cos of the angle that the touched point is at relative to the center of the joystick
+            val hypotenuse = Math.sqrt(
+                Math.pow(
+                    newX - centerX.toDouble(),
+                    2.0
+                ) + Math.pow(newY - centerY.toDouble(), 2.0)
+            ).toFloat()
+            val sin = (newY - centerY) / hypotenuse //sin = o/h
+            val cos = (newX - centerX) / hypotenuse //cos = a/h
+
             //Draw the base first before shading
-            colors.setARGB(255, 32, 32, 32)
+            colors.setARGB(255, 100, 100, 100)
             myCanvas.drawCircle(centerX, centerY, baseRadius, colors)
+            for (i in 1..(baseRadius / ratio).toInt()) {
+                colors.setARGB(
+                    150 / i,
+                    255,
+                    0,
+                    0
+                ) //Gradually decrease the shade of black drawn to create a nice shading effect
+                myCanvas.drawCircle(
+                    newX - cos * hypotenuse * (ratio / baseRadius) * i,
+                    newY - sin * hypotenuse * (ratio / baseRadius) * i,
+                    i * (hatRadius * ratio / baseRadius),
+                    colors
+                ) //Gradually increase the size of the shading effect
+            }
 
-            colors.setARGB(255, 95,95,95)
-            myCanvas.drawCircle(centerX, centerY, hatRadius, colors)
-
+            //Drawing the joystick hat
+            for (i in 0..(hatRadius / ratio).toInt()) {
+                colors.setARGB(
+                    255,
+                    (i * (255 * ratio / hatRadius)).toInt(),
+                    (i * (255 * ratio / hatRadius)).toInt(),
+                    255
+                ) //Change the joystick color for shading purposes
+                myCanvas.drawCircle(
+                    newX,
+                    newY,
+                    hatRadius - i.toFloat() * ratio / 2,
+                    colors
+                ) //Draw the shading for the hat
+            }
             holder.unlockCanvasAndPost(myCanvas) //Write the new drawing to the SurfaceView
         }
     }
@@ -85,48 +124,35 @@ class MyJoystick : SurfaceView, SurfaceHolder.Callback, OnTouchListener {
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {}
-
-
     override fun onTouch(v: View, e: MotionEvent): Boolean {
         if (v == this) {
             if (e.action != MotionEvent.ACTION_UP) {
-                val x1: Float = e.rawX - MotionEvent.AXIS_X
-                val y1: Float = e.rawY - MotionEvent.AXIS_Y
-                val inLim: Float = sqrt(x1 * x1 + y1 * y1)
-                var knobPositionX: Float = x1
-                var knobPositionY: Float = y1
-
-                if (inLim < baseRadius/2) {
-                    knobPositionX = x1
-                    knobPositionY = y1
+                val displacement = Math.sqrt(
+                    Math.pow(
+                        e.x - centerX.toDouble(),
+                        2.0
+                    ) + Math.pow(e.y - centerY.toDouble(), 2.0)
+                ).toFloat()
+                if (displacement < baseRadius) {
+                    drawJoystick(e.x, e.y)
+                    joystickCallback!!.onJoystickMoved(
+                        (e.x - centerX) / baseRadius,
+                        (e.y - centerY) / baseRadius,
+                        id
+                    )
                 } else {
-                    if (x1 == 0f) {
-                        knobPositionY = baseRadius/2
-                        if (y1 < 0) {
-                            knobPositionY *= -1
-                        }
-                    } else {
-                        val m = (y1 / x1).toDouble()
-                        val a = m * m + 1
-                        val b = 2 * m * y1 - 2 * m * m * x1
-                        val c: Double =
-                            m * m * x1 * x1 - 2 * m * y1 * x1 + y1 * y1 - baseRadius * baseRadius/4
-
-                        knobPositionX = if(x1 > 0) {
-                            ((-b + sqrt(b * b - 4 * a * c)) / (2 * a)).toFloat()
-                        } else {
-                            ((-b - sqrt(b * b - 4 * a * c)) / (2 * a)).toFloat()
-                        }
-                        knobPositionY = (m * (knobPositionX - x1) + y1).toFloat()
-                    }
+                    val ratio = baseRadius / displacement
+                    val constrainedX = centerX + (e.x - centerX) * ratio
+                    val constrainedY = centerY + (e.y - centerY) * ratio
+                    drawJoystick(constrainedX, constrainedY)
+                    joystickCallback!!.onJoystickMoved(
+                        (constrainedX - centerX) / baseRadius,
+                        (constrainedY - centerY) / baseRadius,
+                        id
+                    )
                 }
-                drawJoystick(knobPositionX, knobPositionY)
-                joystickCallback!!.onJoystickMoved((knobPositionX - centerX)/baseRadius,
-                    (knobPositionY - centerY)/baseRadius, getId());
-            } else {
-                drawJoystick(centerX, centerY)
-                joystickCallback!!.onJoystickMoved(0f, 0f, id)
-            }
+            } else drawJoystick(centerX, centerY)
+            joystickCallback!!.onJoystickMoved(0f, 0f, id)
         }
         return true
     }
