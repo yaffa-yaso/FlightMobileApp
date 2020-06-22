e11977979b122f6b941911d581caa02bcb6155dc
yaffa <yaffa@DESKTOP-VRQ5167>
2020-06-22 17:36:52 +0300

    main activity

--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -7,9 +7,10 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +20,19 @@ class MainActivity : AppCompatActivity() {
         goToControlScreen.setOnClickListener {
             startActivity(Intent(this, ControlScreenActivity::class.java))
         }
+    }
+
+    override fun onClick(v: View) {
+        /*if(v.id == R.id.localhost1) {
+            R.id.URLText.
+        }else if(v.id == R.id.localhost2){
+            btn2.setText("X");
+        }else if(v.id == R.id.localhost3){
+            btn2.setText("X");
+        }else if(v.id == R.id.localhost4){
+            btn2.setText("X");
+        }else if(v.id == R.id.localhost5){
+            btn2.setText("X");
+        }*/
     }
 }
